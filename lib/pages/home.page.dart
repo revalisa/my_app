639698a70006aed55144c13925fca9bb93195ffe
revalisa <sitirevalisa14@gmail.com>
2026-04-17 12:14:34 +0700

    text baru

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,6 +7,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      // fungsi untuk membuat halaman bisa discroll
       child: SafeArea(
         child: Column(
           children:[
@@ -13,17 +15,35 @@ class HomePage extends StatelessWidget {
               padding: const EdgeInsets.all(16),
               child: Container(
                 child: Row(children: [
+                  // row berfungsi untuk membuat layout mendatar
                   Row(
                     children: [
                       Container(
-                        child: Icon(Icons.download, color: Colors.white,),
+                        child: Icon(Icons.download, color: Colors.white),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8))
                         ),
-                      SizedBox(width: 16,),
+                      SizedBox(
+                        width: 15,
+                      ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text('Hello, User!'),
-                          Text('Welcome Back'),
+                            Text(
+                              "Income",
+                              style: GoogleFonts.montserrat(
+                                color: Colors.white, fontSize: 12,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "\$ 12.000",
+                              style: GoogleFonts.montserrat(
+                                color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold
+                              ),
+                      
+                          )
                         ],
                       )
                   ]
