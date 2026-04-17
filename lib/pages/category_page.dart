79610ba3a8f903299d129ea26c3cbe692678b75c
revalisa <sitirevalisa14@gmail.com>
2026-04-17 14:18:30 +0700

    ubah

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:calendar_appbar/calendar_appbar.dart';
 
 class CategoryPage extends StatefulWidget {
   const CategoryPage({super.key});
@@ -11,41 +10,6 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CalendarAppBar(
-        accent: const Color.fromARGB(255, 176, 119, 138),
-        backButton: false,
-        locale: 'id',
-        onDateChanged: (value) =>
-          print(value),
-          firstDate: DateTime.now().subtract(Duration(days:140)),
-          lastDate: DateTime.now(),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed:(){},
-        backgroundColor: const Color.fromARGB(255, 220, 129, 160), 
-        child: Icon(Icons.add, color: Colors.white,),
-      ),
-
-      body: CategoryPage(),
-
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-        IconButton(onPressed:(){}, icon: Icon(Icons.home)),
-        SizedBox(
-          width: 20,
-        ),
-        IconButton(onPressed:(){
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => CategoryPage()),
-          );
-        }, icon: Icon(Icons.list)),
-      ],),),
-    );
+    return const Placeholder();
   }
 }
