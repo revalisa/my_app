b8112395c7bb525973df376f984cd1366f9a5c9a
revalisa <sitirevalisa14@gmail.com>
2026-04-18 10:26:48 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/pages/main_pages.dart';
-import 'package:flutter/cupertino.dart';
+
 
 void main() {
   runApp(const MyApp());
--- lib/pages/transaction_page.dart
+++ lib/pages/transaction_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TransactionPage extends StatefulWidget {
   const TransactionPage({super.key});
@@ -8,15 +9,72 @@ class TransactionPage extends StatefulWidget {
 }
 
 class _TransactionPageState extends State<TransactionPage> {
+ bool isExpense = true;
+ List<String> list = ['Makan dan Jajan', 'Transportasi', 'film'];
+  late String dropDownValue = list.first;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Add Transaction")),
       body: SingleChildScrollView(
-        child: SafeArea(child: Column(
-          children: [],
-        )),
-      )
-    );
+        child: SafeArea(
+            child: Column(
+              // crossAxis untuk mengatur perataan pada kiri kanan
+              // mainAxisAlignment atas bawah
+              crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Switch(
+                  value: isExpense, 
+                  onChanged: (bool value) {
+                    setState(() {
+                      isExpense = value;
+                    });
+                  } , 
+                  // innactiveTrackColor untuk warna track saat switch dalam keadaan off, inactiveThumbColor untuk warna thumb saat switch dalam keadaan off, activeColor untuk warna thumb saat switch dalam keadaan on
+                  inactiveTrackColor: const Color.fromARGB(255, 198, 235, 198), 
+                  inactiveThumbColor: const Color.fromARGB(255, 120, 218, 118),
+                  // activecolor untuk warna thumb saat switch dalam keadaan on
+                  activeThumbColor: const Color.fromARGB(255, 200, 99, 71),
+                ),
+                Text(isExpense ? 'Expense' :'Income', 
+                style: GoogleFonts.montserrat(fontSize: 14),)
+              ],
+            ),
+            SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: "Ammout"),
+              ),
+            ),
+            SizedBox(height: 25,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+
+              child: Text('Category', 
+                  style: GoogleFonts.montserrat(fontSize: 16,),),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: DropdownButton<String>(
+                value: dropDownValue,
+                isExpanded: true,
+                icon: Icon(Icons.arrow_back),
+                items: list.map<DropdownMenuItem<String>>((String value){
+                  return DropdownMenuItem<String>(
+                  value: value, 
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (String ? value){}),
+            )
+          ],
+      ))),
+      );
   }
 }
