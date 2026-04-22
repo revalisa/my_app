978a2be40d70752457c1111073dd99fc2cac801b
revalisa <sitirevalisa14@gmail.com>
2026-04-22 18:53:35 +0700

    model baru

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -5,8 +5,9 @@ import 'package:drift/drift.dart';
 class Categories extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get name => text().withLength(max: 128)();
+  IntColumn get type => integer()();
   DateTimeColumn get createdAt => dateTime()();
-  DateTimeColumn get updateddAt => dateTime()();
+  DateTimeColumn get updatedAt => dateTime()();
   DateTimeColumn get deletedAt => dateTime().nullable()();
 
 }
--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -3,11 +3,11 @@ import 'package:drift/drift.dart';
 class Transactions extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get name => text().withLength(max: 128)();
-  IntColumn get categori_Id => integer()();
+  IntColumn get category_Id => integer()();
   DateTimeColumn get transaction_date => dateTime()();
   IntColumn get amount => integer()();
   DateTimeColumn get createdAt => dateTime()();
-  DateTimeColumn get updateddAt => dateTime()();
+  DateTimeColumn get updatedAt => dateTime()();
   DateTimeColumn get deletedAt => dateTime().nullable()();
 
 }
